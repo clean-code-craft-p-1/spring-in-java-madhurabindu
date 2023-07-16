d15374d706feb741ca0dc1853e4694c4fa48d967
Rahul Bindu <madhura.bindu@gmail.com>
2023-07-16 05:44:51 +0530

    first

--- src/main/java/statisticker/Statistics.java
+++ src/main/java/statisticker/Statistics.java
@@ -4,7 +4,11 @@ import java.util.List;
 
 public class Statistics 
 {
-    public static Stats getStatistics(List<___> numbers) {
+    public static void getStatistics(List<Float> numbers) {
         //implement the computation of statistics here
+    }
+
+    public static void main(String[] args) {
+        
     }
 }
